253e367b94e957370c85ebb653fd3362d1cccfd2
codage-amour <palak22154041@akgec.ac.in>
2023-10-15 23:04:14 +0530

    second commit

--- api.js
+++ api.js
@@ -1,15 +1,25 @@
-const url = 'https://wordsapiv1.p.rapidapi.com/words/hello/typeOf';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7c1d426238mshd54ba47afaaefd5p110884jsn6e1e6cb0cfd0',
-		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-	}
-};
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('search').addEventListener('click', function() {
+        const word = document.getElementById('box').value;
+        fetchData(word);
+    });
+    
+    async function fetchData(box) {
+        const url = `https://api.api-ninjas.com/v1/thesaurus?word=${box}`;
+        const options = {
+            method: 'GET'
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+			const h=document.getElementById('ans');
+            h.innerHTML = result;
+			h.classList.add('hh'); 
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
+
+
