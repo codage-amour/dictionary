7602561c32d6a244b7fe35b12003b13af4f0f752
codage-amour <palak22154041@akgec.ac.in>
2023-10-17 07:22:43 +0530

    final commit

--- api.js
+++ api.js
@@ -16,6 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
 			const h=document.getElementById('ans');
             h.innerHTML = result;
 			h.classList.add('hh'); 
+			const downloadLink = document.getElementById('down');
+            downloadLink.href = `data:text/plain;charset=utf-8,${encodeURIComponent(result)}`;
+            downloadLink.download = 'downloaded_data.txt';
+			const downLink = document.getElementById('down');
+            downLink.addEventListener('click', function() {
+                downloadLink.click();});
         } catch (error) {
             console.error(error);
         }
